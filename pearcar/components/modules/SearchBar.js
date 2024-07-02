5f34d96e5744c70ff6416c11786804af741cc320
Roozbeh Rigi Jangjo <silentroozbeh@gmail.com>
2024-07-02 21:16:40 +0330

    Feature: The logic of searchBar is add.

--- pearcar/components/modules/SearchBar.js
+++ pearcar/components/modules/SearchBar.js
@@ -1,15 +1,38 @@
-import styles from '../../styles/SearchBar.module.scss'
+import { useState } from "react";
+import { useRouter } from "next/router";
+import styles from "../../styles/SearchBar.module.scss";
 
 function SearchBar() {
+    const router = useRouter();
+
+  const [min, setMin] = useState("");
+  const [max, setMax] = useState("");
+
+  const searchHandler = () => {
+    if (min && max) {
+        router.push(`/filter/${min}/${max}`)
+    } else {
+      alert("Please Enter max and min Price.");
+    }
+  };
+
   return (
     <div className={styles.container}>
-        <div>
-            <input placeholder='Inter min-Price'/>
-            <input placeholder='Inter Max-Price'/>
-        </div>
-        <button>Search</button>
+      <div>
+        <input
+          placeholder="Enter min-Price"
+          value={min}
+          onChange={(e) => setMin(e.target.value)}
+        />
+        <input
+          placeholder="Enter Max-Price"
+          value={max}
+          onChange={(e) => setMax(e.target.value)}
+        />
+      </div>
+      <button onClick={searchHandler}>Search</button>
     </div>
-  )
+  );
 }
 
-export default SearchBar
+export default SearchBar;
