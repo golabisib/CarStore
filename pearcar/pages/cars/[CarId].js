09a1f1546eb5ffd5f316052f2e9438f46db18abb
Roozbeh Rigi Jangjo <silentroozbeh@gmail.com>
2024-07-02 13:26:18 +0330

    Fix: DetailOdCar Page with styles of this are add.

--- pearcar/pages/cars/[CarId].js
+++ pearcar/pages/cars/[CarId].js
@@ -1,12 +1,12 @@
+import DetailOfCar from "@/components/templates/DetailOfCar";
 import carsData from "@/data/carsData";
 import { useRouter } from "next/router";
 
 function CarDetail() {
   const router = useRouter();
   const { CarId } = router.query;
-  const carDerails = carsData[CarId - 1]
-  console.log(carDerails)
-  return <div></div>;
+  const carDetails = carsData[CarId - 1];
+  return <DetailOfCar {...carDetails} />;
 }
 
 export default CarDetail;
